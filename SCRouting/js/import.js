296f0c5c934aba50cf1f5c685693b94c07932eda
RayKeaton <raykeaton2919@gmail.com>
2022-12-10 11:23:01 +0100

    update

--- SCRouting/js/import.js
+++
@@ -1,7 +0,0 @@
-import Feature from 'ol/Feature.js';
-import Map from 'ol/Map.js';
-import View from 'ol/View.js';
-import Point from 'ol/geom/Point.js';
-import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
-import {Cluster, OSM, Vector as VectorSource} from 'ol/source.js';
-import {Circle as CircleStyle, Fill, Stroke, Style, Text} from 'ol/style.js';
